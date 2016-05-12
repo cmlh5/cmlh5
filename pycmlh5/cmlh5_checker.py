f3d747c3ff3f244d534a76a851a18615063144e1
Christian Chwala <christian.chwala@kit.edu>
2016-05-12 12:29:44 +0200

    cmlh5_checker.py is now working

--- pycmlh5/cmlh5_checker.py
+++ pycmlh5/cmlh5_checker.py
@@ -11,17 +11,36 @@
 # Licence:      The MIT License
 #----------------------------------------------------------------------------
 
-import sys
-
-#import os
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import argparse
 
 from pycmlh5.metadata_def_parser import read_and_check_metadata
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Validate cmlh5 file')
+    parser.add_argument('file_name',
+                        help='Name of the file to validate')
+    parser.add_argument('--less_strict_type_check', action='store_true',
+                        help='Do not check for float precision')
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == "__main__":
-    fn = sys.argv[0]
-    print fn
-    cml_metadata_list, error_list = read_and_check_metadata(fn)
+    args = parse_args()
+
+    print '\n###################################################################'
+    print ' Checking file %s' % args.file_name
+    print '###################################################################\n'
+    cml_metadata_list, error_list = read_and_check_metadata(fn=args.file_name,
+                                                            strict_type_check=args.less_strict_type_check)
+    for error in error_list:
+        print error
+
+    print '\n Found %d errors \n' % len(error_list)
+
+
+
 
 
 
